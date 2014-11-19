a95239bc64589046b6d945f0c8a03b97f8f87b53
Tyler Johnson <tyler@vintyge.com>
2014-11-18 22:25:08 -0700

    reorganized repetitive test code

--- test/test.js
+++ test/test.js
@@ -1,19 +1,24 @@
 describe("Single Model Symlinks", function() {
+	var model1, model2, col;
+
+	beforeEach(function() {
+		model1 = new Backbone.Model({ foo: "2", id: "1" });
+		model2 = new Backbone.Model({ id: "2" });
+		col = new Backbone.Collection();
+	});
+
+	afterEach(function() {
+		model1.unlink();
+		col.reset();
+	});
 
 	it("basic symlink", function() {
-		var model1 = new Backbone.Model({ foo: "2", id: "1" }),
-			model2 = new Backbone.Model({ id: "2" }),
-			col = new Backbone.Collection([ model2 ]);
-
+		col.add(model2);
 		model1.symlink("foo", col);
 		expect(model1.get("foo")).to.equal(model2);
 	});
 
 	it("symlinks to model before model is in collection", function() {
-		var model1 = new Backbone.Model({ foo: "2", id: "1" }),
-			model2 = new Backbone.Model({ id: "2" }),
-			col = new Backbone.Collection();
-
 		model1.symlink("foo", col);
 		expect(model1.get("foo")).to.equal(null);
 		expect(model1.getSymlink("foo").valid).to.equal(false);
@@ -24,9 +29,7 @@ describe("Single Model Symlinks", function() {
 	});
 
 	it("unlinks", function() {
-		var model1 = new Backbone.Model({ foo: "2", id: "1" }),
-			model2 = new Backbone.Model({ id: "2" }),
-			col = new Backbone.Collection([ model2 ]);
+		col.add(model2);
 
 		model1.symlink("foo", col);
 		expect(model1.get("foo")).to.equal(model2);
@@ -36,10 +39,6 @@ describe("Single Model Symlinks", function() {
 	});
 
 	it("announces when a delayed model arrives", function(done) {
-		var model1 = new Backbone.Model({ foo: "2", id: "1" }),
-			model2 = new Backbone.Model({ id: "2" }),
-			col = new Backbone.Collection();
-
 		model1.symlink("foo", col);
 		expect(model1.get("foo")).to.equal(null);
 		expect(model1.getSymlink("foo").valid).to.equal(false);
@@ -54,10 +53,7 @@ describe("Single Model Symlinks", function() {
 	});
 
 	it("toJSON does not include modes, only the id", function() {
-		var model1 = new Backbone.Model({ foo: "2", id: "1" }),
-			model2 = new Backbone.Model({ id: "2" }),
-			col = new Backbone.Collection([ model2 ]);
-
+		col.add(model2);
 		model1.symlink("foo", col);
 		expect(model1.toJSON()).to.deep.equal({ foo: model2.id, id: model1.id });
 	});
@@ -65,13 +61,24 @@ describe("Single Model Symlinks", function() {
 });
 
 describe("Multiple Model Symlinks", function() {
+	var main, sub1, sub2, sub3, col;
+
+	beforeEach(function() {
+		main = new Backbone.Model({ foo: [], id: "1" });
+		sub1 = new Backbone.Model({ id: "2" });
+		sub2 = new Backbone.Model({ id: "3" });
+		sub3 = new Backbone.Model({ id: "4" });
+		col = new Backbone.Collection();
+	});
+
+	afterEach(function() {
+		main.unlink();
+		col.reset();
+	});
 
 	it("symlinks to list of models", function() {
-		var main = new Backbone.Model({ foo: [ "2", "4" ], id: "1" }),
-			sub1 = new Backbone.Model({ id: "2" }),
-			sub2 = new Backbone.Model({ id: "3" }),
-			sub3 = new Backbone.Model({ id: "4" }),
-			col = new Backbone.Collection([ sub1, sub2, sub3 ]);
+		main.set("foo", [ "2", "4" ]);
+		col.add([ sub1, sub2, sub3 ]);
 
 		main.symlink("foo", col);
 		expect(main.get("foo")).to.be.instanceof(Backbone.Collection);
@@ -79,11 +86,8 @@ describe("Multiple Model Symlinks", function() {
 	});
 
 	it("symlinks to list of models before models are in collection", function() {
-		var main = new Backbone.Model({ foo: [ "2", "4" ], id: "1" }),
-			sub1 = new Backbone.Model({ id: "2" }),
-			sub2 = new Backbone.Model({ id: "3" }),
-			sub3 = new Backbone.Model({ id: "4" }),
-			col = new Backbone.Collection([ sub3 ]);
+		main.set("foo", [ "2", "4" ]);
+		col.add([ sub3 ]);
 
 		main.symlink("foo", col);
 		expect(main.get("foo")).to.be.instanceof(Backbone.Collection);
@@ -97,11 +101,8 @@ describe("Multiple Model Symlinks", function() {
 	});
 
 	it("unlinks", function() {
-		var main = new Backbone.Model({ foo: [ "2", "3", "4" ], id: "1" }),
-			sub1 = new Backbone.Model({ id: "2" }),
-			sub2 = new Backbone.Model({ id: "3" }),
-			sub3 = new Backbone.Model({ id: "4" }),
-			col = new Backbone.Collection([ sub1, sub3 ]);
+		main.set("foo", [ "2", "3", "4" ]);
+		col.add([ sub1, sub3 ]);
 
 		main.symlink("foo", col);
 		expect(main.get("foo")).to.be.instanceof(Backbone.Collection);
@@ -110,6 +111,17 @@ describe("Multiple Model Symlinks", function() {
 
 		main.unlink("foo");
 		expect(main.get("foo")).to.deep.equal([ "2", "3", "4" ]);
+	});
+
+	it("ids of models added to subset appear in real value", function() {
+		col.add([ sub1 ]);
+
+		main.symlink("foo", col);
+		expect(main.get("foo")).to.be.instanceof(Backbone.Collection);
+		expect(main.get("foo").length).to.equal(0);
+
+		main.get("foo").add(sub1);
+		expect(main.deref("foo")).to.deep.equal([ sub1.id ]);
 	});
 
 });
